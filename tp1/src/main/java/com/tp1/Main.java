6064bf2121d3d98b104da29a0032a1102ec240b8
Uriel Degrugillier <urieldegrugillier@gmail.com>
2026-09-09 18:18:59 +0200

    Ajout classe Main avec affichage calculatrice

--- tp1/src/main/java/com/tp1/Main.java
+++ tp1/src/main/java/com/tp1/Main.java
@@ -3,5 +3,6 @@ package com.tp1;
 public class Main {
     public static void main(String[] args) {
         System.out.println("Calculatrice");
+        System.out.println("Addition fonctionnelle dans la calculatrice");
     }
 }
